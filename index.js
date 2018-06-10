b510b0e30e6a69b013f938be18d4ea47163f1699
jsgilberto <alv.mtz94@gmail.com>
2018-06-10 23:28:53 +0200

    first version of youtube-frames

---
+++ index.js
@@ -0,0 +1,153 @@
+// Immediately Invoked Function Expression
+(function(g){
+  var ytdl = require('ytdl-core');
+  var fs = require('fs');
+  var spawn = require('child_process').spawn;
+
+  const $ytvideo = function(youtubeURL, videoName){
+
+    return new $ytvideo.init(youtubeURL, videoName);
+
+  };
+
+  // safely create directory
+  const mkdirSync = function (dirPath) {
+    try {
+      fs.mkdirSync(dirPath);
+    } catch (err) {
+      console.log("DIR EXISTS");
+      if (err.code !== 'EEXIST')
+        throw err;
+    }
+  };
+
+
+  $ytvideo.prototype = {
+    // Download video from YouTube
+    download: function(path){
+      var self = this;
+      self.path = path || './';
+
+      if (self.path !== './' && self.path){
+        mkdirSync(self.path);
+      }
+      
+      // create a promise
+      self.prom = new Promise(function(resolve){
+
+        let video = ytdl(self.youtubeURL);
+        let vpipe = video.pipe(fs.createWriteStream(`${self.path}/${self.videoName}.mp4`));
+        
+        video.on('progress', function(chunkLength, downloaded, total){
+
+          const floatDownloaded = downloaded / total;
+          console.log(`${self.videoName} - Download progress: ${parseFloat(floatDownloaded*100).toFixed(2)} %`);
+
+        });
+
+        video.on('error', function(err){
+
+          console.log("\nThere was an error downloading.\n");
+          throw err;
+
+        });
+
+        video.on('end', function(){
+
+          console.log(`\nFinished downloading: ${self.videoName}\n`);
+
+        });
+
+        vpipe.on('finish', function(){
+
+          console.log(`Finished writing ${self.videoName}.mp4 to disk`);
+          resolve();
+
+        });
+        /* videoWriteStream.on('open', (data) => {
+          console.log(`Downloading ${self.videoName}`);
+        });
+        videoWriteStream.on('error', (err) => {
+          console.log("ERROR!!!!!");
+        });
+        videoWriteStream.on('close', () => {
+          console.log(`Finished downloading ${self.videoName}`);
+          resolve();
+        }); */
+      });
+      return self;
+    },
+
+    toFrames: function(fps){
+      var self = this;
+      console.log(self);
+      self.convertToFrames(fps, self.prom);
+    },
+
+    convertToFrames: function(fps, promise) {
+      
+      var self = this;
+      
+      fps = fps || 1
+      if (typeof fps !== 'number') {
+        throw "fps argument must be a number"
+      }
+      else {
+        String(fps);
+      }
+  
+      function getFrames(){
+
+        console.log(`Video Frame Process of ${self.videoName}`);
+
+        let ffmpegVideoFrameProcess = spawn('ffmpeg', [
+          '-i', `${self.path}/${self.videoName}.mp4`,
+          '-f', 'image2',
+          '-bt', '20M',
+          '-vf', `fps=${fps}`,
+          `${self.path}/${self.videoName}%03d.jpg`,
+          '-loglevel', 'panic',
+          '-nostdin'
+        ]);
+        /* ffmpegVideoFrameProcess.stdout.on('data', (data) => {
+          console.log(data.toString());
+        });
+      
+        ffmpegVideoFrameProcess.stderr.on('error', (err) => {
+          console.log(err.toString());
+        });
+      
+        ffmpegVideoFrameProcess.stdout.on('close', (data) => {
+          process.exit()
+        }); */
+      }
+
+      if(promise){
+        promise.then(getFrames);
+      }
+      
+    }
+  };
+
+  // this is the object created when you call $ytvideo
+  $ytvideo.init = function(youtubeURL, videoName, path){
+    this.youtubeURL = youtubeURL;
+    this.videoName = videoName || 'video';
+    this.path = path || './';
+  }
+
+  $ytvideo.init.prototype = $ytvideo.prototype;
+
+  g.$ytvideo = g.$yt = $ytvideo;
+
+}(global));
+
+let o = $ytvideo('https://www.youtube.com/watch?v=sDj72zqZakE', 'waffle_falling');
+let p = $ytvideo('https://www.youtube.com/watch?v=3ueqncw103A', 'bread_falling');
+let k = $ytvideo('https://www.youtube.com/watch?v=B7bqAsxee4I', 'person_falling')
+o.download('./images1').toFrames();
+p.download('./images2').toFrames();
+k.download('./images3').toFrames();
+
+
+
